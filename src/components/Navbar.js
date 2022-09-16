4b2ae12803da03aed9ca9d756e5ae2040c8cc77b
user name <user email>
2022-09-16 18:38:21 +0530

    nav bar updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
     return (
-      <div className="navbar">
-        <span className="logo">Lama Chat</span>
-        <div className="user">
-          <img
-            src="https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt=""
-          />
-          <span>Jhon</span>
-          <button>logout</button>
+        <div className="navbar">
+            <span className="logo">Lama Chat</span>
+            <div className="user">
+                <img
+                    src="https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    alt="no-img"
+                />
+                <span>Jhon</span>
+                <button>logout</button>
+            </div>
         </div>
-      </div>
     );
-}
+};
 
-export default Navbar
+export default Navbar;
